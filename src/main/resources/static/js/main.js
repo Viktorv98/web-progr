4070c609af0d407bfacb929d41dd9783a585758a
Виктор <viktor.zv1@yandex.ru>
2019-12-17 03:27:15 +0500

    Add sort for cost

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,34 +1,37 @@
-function setString(){
-var inputDate = document.getElementById('poi');
-return inputDate.value;
-}
 
-function clikButtonFiltr(){
-var str = setString();
-window.alert(str);
+function mySort(){
+var elem = document.querySelector('.products_clearfix');
+for (var i = 0; i < elem.children.length; i++){
+  for (var j = i; j < elem.children.length; j++){
+     if (+elem.children[i].children[0].children[1].children[2].innerText > +elem.children[j].children[0].children[1].children[2].innerText){
+        elemDelete = elem.replaceChild(elem.children[j], elem.children[i]);
+        elem.children[i].parentNode.insertBefore(elemDelete,elem.children[i].nextSibling);
+     }
+  }
+}
 }
 
 var sendButtonFiltr = document.getElementById('filtr');
-sendButtonFiltr.addEventListener('click',clikButtonFiltr);
+sendButtonFiltr.addEventListener('click',mySort);
 
 document.querySelector('#poi').oninput = function(){
-let val = this.value.trim();
-let elasticItems = document.querySelectorAll('.products_clearfix li');
+  var val = this.value.trim();
+  var elasticItems = document.querySelectorAll('.products_clearfix li');
 
-if (val != '')
-{
-  elasticItems.forEach(function(elem){
-  if (elem.children[0].children[1].children[0].innerText.search(val) == -1){
-  elem.classList.add('hide');
+  if (val != '')
+  {
+     elasticItems.forEach(function(elem){
+        if (elem.children[0].children[1].children[0].innerText.search(val) == -1){
+           elem.classList.add('hide');
+        }
+        else{
+           elem.classList.remove('hide');
+        }
+     });
   }
   else{
-  elem.classList.remove('hide');
+     elasticItems.forEach(function(elem){
+        elem.classList.remove('hide');
+     });
   }
-  });
-}
-else{
-elasticItems.forEach(function(elem){
-elem.classList.remove('hide');
-});
-}
 }
