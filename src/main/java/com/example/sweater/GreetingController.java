41ed3957d27e8c63dd73d250ddc655a8d03d0a55
Виктор <viktor.zv1@yandex.ru>
2019-12-17 16:56:18 +0500

    Add cookie

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -3,11 +3,14 @@ package com.example.sweater;
 import com.example.sweater.models.Items;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.example.sweater.repos.ItemsRepo;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 @Controller
@@ -16,10 +19,15 @@ public class GreetingController {
     private ItemsRepo itemsRepo;
     Iterable<Items> items;
 
-    @GetMapping("/greeting")
-    public String greeting (Map<String, Object> model, HttpServletRequest request){
+    @GetMapping("/")
+    public String greeting (Map<String, Object> model, HttpServletResponse response, @CookieValue (value = "myCity", defaultValue = "null") String myCookie){
         items = itemsRepo.findAll();
         model.put( "items", items);
+        Cookie cookie = new Cookie("myCity", myCookie);
+        cookie.setMaxAge(-1);
+        response.addCookie(cookie);
+        model.put("city",myCookie);
+
         return "greeting";
     }
 
